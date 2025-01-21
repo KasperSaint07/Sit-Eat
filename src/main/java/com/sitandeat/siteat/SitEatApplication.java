0772558219e5d0f4ef7784ccb50b520a49c95d14
madiy <madiyar20001234@gmail.com>
2025-01-21 15:34:51 +0500

    Initial setup of Sit&Eat project: added REST API structure, database configuration, and basic endpoints

---
+++ src/main/java/com/sitandeat/siteat/SitEatApplication.java
@@ -0,0 +1,13 @@
+package com.sitandeat.siteat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SitEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sitandeat/siteat/SitEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitandeat.siteat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
